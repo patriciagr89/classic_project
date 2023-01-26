515e756caafabcbb5e3d6e4ffc06cb3978d885a7
Patricia Gutierrez <patriciagr@gmx.es>
2023-01-26 23:26:54 +0100

    disabled menu link in the same route

--- app_cripto/routes.py
+++ app_cripto/routes.py
@@ -5,14 +5,14 @@ from app_cripto.models import *
 @app.route("/")
 def index():
     registros = select_all()
-    return render_template("index.html", data = registros)
+    return render_template("index.html", data = registros, isIndex = True)
 
 @app.route("/purchase")
 def purchase():
-    return render_template("purchase.html")
+    return render_template("purchase.html", isPurchase = True)
 
 @app.route("/status")
 def status():
     status = select_status()
-    return render_template("status.html", data = status)
+    return render_template("status.html", data = status, isStatus = True)
 

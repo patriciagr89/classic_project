a3a33eed7f5f435102621fb36243ddc304ff3c85
Patricia Gutierrez <patriciagr@gmx.es>
2023-01-28 14:11:28 +0100

    empty movements message

--- app_cripto/routes.py
+++ app_cripto/routes.py
@@ -1,11 +1,11 @@
 from app_cripto import app
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from app_cripto.models import *
 
 @app.route("/")
 def index():
     registros = select_all()
-    return render_template("index.html", data = registros, isIndex = True)
+    return render_template("index.html", movements = registros, isIndex = True)
 
 @app.route("/purchase", methods = ["POST","GET"])
 def purchase():
@@ -14,5 +14,5 @@ def purchase():
 @app.route("/status")
 def status():
     status = select_status()
-    return render_template("status.html", data = status, isStatus = True)
+    return render_template("status.html", movements = status, isStatus = True)
 

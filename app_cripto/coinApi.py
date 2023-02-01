e07fdc655fad004a912cefb53b79a1c3ec9f6103
Patricia Gutierrez <patriciagr@gmx.es>
2023-02-01 23:02:14 +0100

    class CoinApi create

--- app_cripto/coinApi.py
+++ app_cripto/coinApi.py
@@ -1,12 +1,13 @@
 import requests
+from config import *
 
 class CoinApi:
-    def __init__(self, apiKey, coin_from, coin_to):
+    def __init__(self, APIKEY, coin_from, coin_to):
         self.coin_from = coin_from
         self.coin_to = coin_to
         self.rate = None
         self.time = None
-        self.url = "https://rest.coinapi.io/v1/exchangerate/{coin_from}/{coin_to}?time={time}&apikey={apiKey}"
+        self.url = "https://rest.coinapi.io/v1/exchangerate/{coin_from}/{coin_to}?&apikey={APIKEY}"
         self.result = requests.get(self.url)
 
     def coinChange(self):
